b5098d9b839b3b0323255c1c84d646e1aadd26bf
tombrown3 <brownotieno500@gmail.com>
2022-10-10 14:50:26 +0300

    change  else conditional statement

--- guessingGame.py
+++ guessingGame.py
@@ -4,9 +4,9 @@ guess_count = 0
 guess_limit = 3
 while guess_count < guess_limit:
     guess=int(input("Enter your guess:"))
+    guess_count = guess_count + 1
     if guess == secret_number:
         print("Your guess is right")
         break
-    elif guess!= secret_number:
-        print("You are wrong try again:")
-    guess_count = guess_count + 1
+else:
+    print("Sorry you are wrong, the right answer is",secret_number)
